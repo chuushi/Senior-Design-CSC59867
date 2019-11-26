4a495332a587678d53ff474ed213700a163cf47b
Charding96 <atsutomo@hotmail.com>
2019-11-26 15:47:24 -0500

    Update gpio file

--- src/receiver/gpio-layer.js
+++ src/receiver/gpio-layer.js
@@ -1,10 +1,10 @@
 const Gpio = require('onoff').Gpio;
 
 const move = {
-    left: new Gpio(17, 'out'),
-    right: new Gpio(17, 'out'),
-    forward: new Gpio(17, 'out'),
-    backward: new Gpio(17, 'out')
+    left: new Gpio(20, 'out'),
+    right: new Gpio(26, 'out'),
+    forward: new Gpio(19, 'out'),
+    backward: new Gpio(16, 'out')
 }
 
 const out = function out( direction ) {
@@ -15,13 +15,35 @@ const out = function out( direction ) {
             move.forward.writeSync(0);
             move.backward.writeSync(0);
             break;
+
         case "left":
+        move.left.writeSync(1);
+        move.right.writeSync(0);
             break;
+
         case "right":
+        move.left.writeSync(0);
+        move.right.writeSync(1);
             break;
         case "forward":
+        move.forward.writeSync(1);
+        move.backward.writeSync(0);
             break;
+
         case "backward":
+        move.forward.writeSync(0);
+        move.backward.writeSync(1);
+            break;
+        
+        case "stand":
+        move.forward.writeSync(0);
+        move.backward.writeSync(0);
+            break;
+        
+        case "no-steer":
+        move.left.writeSync(0);
+        move.right.writeSync(0);
+            break;
     }
     
 }
