0135b9e46db5d382f69d694b7e379f2671b1ce60
Simon Chuu <simonorj@outlook.com>
2019-11-26 17:23:55 -0500

    fix config again again

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     rate: 41000,
     freqs: [20500, 20350, 20200, 20050, 19900],
     sigs: [6, 5, 5, 5, 5],
-    showDebug: true;
+    showDebug: true,
     aliveFreq: 19750,
     aliveSig: 5
 };
